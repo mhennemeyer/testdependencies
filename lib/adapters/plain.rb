981a6fceaa3547db0c0bc7e44ee846b97ff7f1bc
Matthias Hennemeyer <mhennemeyer@me.com>
2010-04-20 20:20:29 +0200

    return testname

--- lib/adapters/plain.rb
+++ lib/adapters/plain.rb
@@ -2,8 +2,10 @@ ActiveSupport::TestCase.module_eval do
   class<<self
     alias old_test test
     def test(decl, &block)
-      unless String === decl
-        old_test decl.keys.first do
+      name = decl
+      if Hash === decl
+        name = decl.keys.first
+        old_test name do
           dep = decl.values.first
           dep = "test_#{dep.gsub(/\s+/,'_')}".to_sym if String === dep
           raise "Dependency undefined: #{dep}" unless respond_to?(dep)
@@ -11,8 +13,9 @@ ActiveSupport::TestCase.module_eval do
           instance_exec(*result, &block) unless result == :__failure_in_dep
         end
       else
-        old_test decl, &block
+        old_test name, &block
       end
+      name
     end
   end
 end
