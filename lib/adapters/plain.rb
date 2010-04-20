7bb15c97037653153888d347e2a953ca0e47a89c
Matthias Hennemeyer <mhennemeyer@me.com>
2010-04-20 13:34:50 +0200

    don't skip the test if eval of dependency returns false

--- lib/adapters/plain.rb
+++ lib/adapters/plain.rb
@@ -7,8 +7,8 @@ ActiveSupport::TestCase.module_eval do
           dep = decl.values.first
           dep = "test_#{dep.gsub(/\s+/,'_')}".to_sym if String === dep
           raise "Dependency undefined: #{dep}" unless respond_to?(dep)
-          result = send(dep) rescue false
-          instance_exec(*result, &block) if result
+          result = send(dep) rescue :__failure_in_dep
+          instance_exec(*result, &block) unless result == :__failure_in_dep
         end
       else
         old_test decl, &block
